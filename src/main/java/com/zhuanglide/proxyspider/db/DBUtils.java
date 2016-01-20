a1271425bc045c779f1a0e30eddd0a2473073233
wangweijie <wangweijie@kanzhun.com>
2016-01-20 14:57:30 +0800

    增加db

---
+++ src/main/java/com/zhuanglide/proxyspider/db/DBUtils.java
@@ -0,0 +1,83 @@
+package com.zhuanglide.proxyspider.db;
+
+import com.zhuanglide.proxyspider.db.mapper.Proxy;
+import com.zhuanglide.proxyspider.db.mapper.ProxyMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ * Created by wwj on 16.1.13.
+ */
+public class DBUtils {
+    private static DBUtils utils;
+    private static ThreadLocal<SqlSession> threadLocal = new ThreadLocal<SqlSession>();
+
+    public SqlSessionFactory sessionFactory;
+    private DBUtils(){
+    }
+
+    public static DBUtils instance() {
+        if(null == utils){
+            synchronized (DBUtils.class){
+                if (null == utils) {
+                    utils = new DBUtils();
+                    String resource = "mybatis-config.xml";
+                    Reader reader = null;
+                    try {
+                        reader = Resources.getResourceAsReader(resource);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    utils.sessionFactory = new SqlSessionFactoryBuilder().build(reader);
+                    utils.init();
+                }
+            }
+        }
+        return utils;
+    }
+
+    private void init(){
+        Configuration config = sessionFactory.getConfiguration();
+        config.addMapper(ProxyMapper.class);
+    }
+
+    public synchronized SqlSession getSqlSession(){
+        SqlSession sqlSession = threadLocal.get();
+        if(null == sqlSession){
+            sqlSession = sessionFactory.openSession();
+            threadLocal.set(sqlSession);
+        }
+        return sqlSession;
+    }
+
+    public synchronized void closeSqlSession(){
+        SqlSession sqlSession = threadLocal.get();
+        if (null != sqlSession) {
+            sqlSession.close();
+        }
+    }
+
+
+    public <T> T getMapper(Class<T> clazz,SqlSession sqlSession) {
+        return sqlSession.getMapper(clazz);
+    }
+
+
+    public static void main(String[] args) {
+        DBUtils dbUtils = DBUtils.instance();
+        SqlSession session = dbUtils.getSqlSession();
+        ProxyMapper proxyMapper = dbUtils.getMapper(ProxyMapper.class,session);
+        Proxy proxy = new Proxy();
+        proxy.setHost("127.0.0.1");
+        proxy.setPort(80);
+        proxyMapper.insert(proxy);
+        session.commit();
+        dbUtils.closeSqlSession();
+    }
+}
